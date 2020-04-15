ee4440988d3e187af3c74a36ff6db52a9dc6d5da
Cody Enokida <codyenokida@gmail.com>
2020-04-14 17:27:08 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const weatherRoute = require('./routes/api/weather')
@@ -10,6 +11,16 @@ app.use(express.json())
 
 app.use('/api/weather', weatherRoute)
 app.use('/api/weekly', weeklyRoute)
+
+// Serve static assets if production
+if (process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
